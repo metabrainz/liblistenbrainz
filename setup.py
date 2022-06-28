8339fc2455b751bcf98a1e8297170ad6d6a1657c
Robert Kaye <rob@musicbrainz.org>
2022-06-28 17:24:42 +0200

    Remove pylistenbrainz refs

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 import setuptools
-import pylistenbrainz
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pylistenbrainz",
-    version=pylistenbrainz.__version__,
     author="Param Singh",
     author_email="iliekcomputers@gmail.com",
     description="A simple ListenBrainz client library for Python",
